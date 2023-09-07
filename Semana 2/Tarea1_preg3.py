64b0253497b44b1d6dd9c24cc625f7936849dd02
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-09-07 09:46:14 -0500

    Tarea1_preg3.py

--- Semana 2/Tarea1_preg3.py
+++ Semana 2/Tarea1_preg3.py
@@ -1,3 +1,4 @@
+#Pregunta 3
 #Cree una función que permita calcular la sumatoria de:
 
 def sumatoria(ini, fin):
@@ -5,7 +6,9 @@ def sumatoria(ini, fin):
     for i in range (ini, fin + 1):
         suma += i
     return suma
-ini = 1
-fin = 5
+ini = int(input("Ingrese el primer numero por favor: "))
+fin = int(input("Ingrese el segundo numero por favor: "))
 
-print(f"La suma de los numeros entre {ini} y {fin} es {sumatoria(ini, fin)}")
+resultado = sumatoria(ini, fin)
+
+print(f"La suma de los numeros entre {ini} y {fin} es {resultado}")
